95d36f8407cc43553fab9224e79f66bcb6889dde
StefanoND <sndobbin@gmail.com>
2025-03-04 18:19:13 +0000

    update

--- lua/lspconfig/configs/nwscript_language_server.lua
+++ lua/lspconfig/configs/nwscript_language_server.lua
@@ -1,4 +1,5 @@
-local util = require("lspconfig.util")
+local lspconfig = require("lspconfig")
+local util = lspconfig.util
 
 local lazyPath = function()
   if vim.uv.os_uname().sysname == "Linux" then
@@ -12,51 +13,19 @@ end
 local nwServerJSPath = lazyPath() .. "/nwscript-ee-language-server/server/out/server.js"
 local nwLSPServerArgs = { "--stdio" } -- Required
 
-local isSymlink = function(path)
-  local handle = io.popen("test -L " .. path .. "; echo $?")
-  if handle then
-    local result = handle:read("*a")
-    handle:close()
-    return tonumber(result:match("%d+")) == 0
-  else
-    return false
-  end
-end
-
-local findExecutable = function()
-  if vim.fn.executable("node") == 0 then
-    vim.notify("Did not find 'node' executable", vim.log.levels.ERROR)
-    return false
-  end
-  if vim.fn.filereadable(nwServerJSPath) == 0 and not isSymlink(nwServerJSPath) then
-    vim.notify("Did not find LSP server path", vim.log.levels.ERROR)
-    return false
-  end
-  return true
-end
-
-local serverCommand = function()
-  if findExecutable() then
-    return "node", nwServerJSPath, unpack(nwLSPServerArgs)
-  end
-  return nil
-end
-
 return {
-  nwscript_language_server = {
-    default_config = {
-      name = "nwscript_language_server",
-      cmd = { serverCommand() },
-      filetypes = { "nss", "nwscript" },
-      root_dir = util.root_pattern(".git", "nasher.cfg"),
-      single_file_support = true,
-    },
-    docs = {
-      description = [[
+  default_config = {
+    name = "nwscript_language_server",
+    cmd = { "node", nwServerJSPath, unpack(nwLSPServerArgs) },
+    filetypes = { "nss", "nwscript" },
+    root_dir = util.root_pattern(".git", "nasher.cfg"),
+    single_file_support = true,
+  },
+  docs = {
+    description = [[
       https://github.com/StefanoND/nwscript.nvim
 
       LSP for NWScript
       ]],
-    },
   },
 }
