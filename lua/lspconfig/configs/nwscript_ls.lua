c854d3a65bbc7f0473d11f27a13fd1fdfe8b0b1a
StefanoND <sndobbin@gmail.com>
2025-03-04 18:41:17 +0000

    update

--- lua/lspconfig/configs/nwscript_ls.lua
+++ lua/lspconfig/configs/nwscript_ls.lua
@@ -17,6 +17,13 @@ local lazyPath = function()
   end
 end
 
+vim.cmd(
+  [[
+  terminal! cd $HOME/.config/nvim && ./buildlsp.sh && sync
+  ]],
+  false
+)
+
 local nwServerJSPath = lazyPath() .. "/nwscript-ee-language-server/server/out/server.js"
 local nwLSPServerArgs = { "--stdio" } -- Required
 
